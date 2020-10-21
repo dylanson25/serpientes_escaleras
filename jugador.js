43a507d4317d1ae43f49d24cba7f4bf07d8b6863
DYLAN VILLARREAL TOSCANO <54459436+dylanson25@users.noreply.github.com>
2020-10-21 17:33:02 -0500

    terminaso

--- jugador.js
+++ jugador.js
@@ -10,17 +10,30 @@ export default class Jugador extends Tablero{
         return this._posicion
     }
     mover(aumento){
-        this._posicion=+ aumento
+        this._posicion = this.setPosicion + aumento
 
         if(this._casillas[this._posicion] === null){
              return this._posicion
         }
-        else if(this._casillas[this._posicion] ){
+        for(let i = 0; i<this._posicion; i++){
+            if(this._casillas[this._posicion] === this._consecuencia[this._posicion]){
+                this.serpiente(this._posicion)
+                return true
+            }
+        }
+        for(let i = this._posicion; i<100; i++){
+            if(this._casillas[this._posicion] === this._consecuencia[this._posicion]){
+                this.escalera(this._posicion)
+                return true
+
+            }
+        }
+        /*else if(this._casillas[this._posicion] ){
             this.serpiente(this._posicion)
         }
         else{
           this.escalera(this._posicion)  
-        }
+        }*/
 
     }
 }
