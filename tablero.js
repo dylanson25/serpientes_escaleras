d999594b2208c4a3935622aa63c7e9c6bcd7605e
DYLAN VILLARREAL TOSCANO <54459436+dylanson25@users.noreply.github.com>
2020-10-21 11:11:50 -0500

    generar tablero
    creo las serpientes y escaleras aleatoriamente y les asigno donde empiezan y donde terminan

--- tablero.js
+++ tablero.js
@@ -1,22 +1,30 @@
 export default class Tablero {
     constructor() {
         this._casillas = new Array(100)
+        this._consecuencia = new Array(100)
     }
     generaTablero() {
-        //cargando serpientes
-        for (let i = 0; i < 10; i++) {
-            let crearSer = Math.ceil(Math.random() * 100)
-            if (this._casillas[crearSer] = 'undefined')
-                this._casillas[crearSer] = 'S'
-        }
-        //cargando serpientes
-        for (let i = 0; i < 10; i++) {
-            let crearEsc = Math.ceil(Math.random() * 100)
-            if (this._casillas[crearEsc] = 'undefined')
-                this._casillas[crearEsc] = 'E'
-        }
+        //A qui todos los espacios de mi vector los reinicio a null
         for (let i = 0; i < 100; i++) {
-            console.log(this._casillas[i] + ' ' + i)
+            this._casillas[i] = null    
+            this._casillas[i] = null
+            this._consecuencia[i] = null
+            this._consecuencia[i] = null
+        }
+        //genero a las serpientes y escaleras
+        for (let i = 0; i < 10; ) {
+            let crearSer = Math.ceil(Math.random() * 100)
+            let crearEsc = Math.ceil(Math.random() * 100)
+            let crearCola = Math.ceil(Math.random() * 100)
+            let crearFin = Math.ceil(Math.random() * 100)
+
+            if (this._casillas[crearEsc] == null && this._consecuencia[crearFin] == null && crearEsc < crearFin && this._casillas[crearSer] == null && this._consecuencia[crearCola] == null && crearSer > crearCola){
+                this._casillas[crearEsc] = 'E' + i
+                this._consecuencia[crearFin] = 'E' + i
+                this._casillas[crearSer] = 'S' + i
+                this._consecuencia[crearCola] = 'S' + i
+                i++
+            }
         }
 
     }
